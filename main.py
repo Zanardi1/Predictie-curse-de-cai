acd2bb9c81d4a03c65e613b17df3231313079216
Bogdan Doicin <alex.zanardi@gmail.com>
2021-11-09 11:12:15 +0200

    Actualizare biblioteci

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
 raw_data = pd.read_excel('Data.xlsx')
-print(raw_data.head())
+# print(raw_data.shape)
+# print(raw_data.head())
+# print(raw_data.isnull().any(axis=1))
+
+horse_data = pd.DataFrame()
+horse_data['Horse ID'] = raw_data['HorseId'].unique()
+# horse_data['Last FGRating'] = raw_data['FGrating']
+print(horse_data.shape)
+print(horse_data.head())
