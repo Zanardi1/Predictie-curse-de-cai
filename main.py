d3e37a6317c5ccf60abedb42e33f6c914904e5a1
Bogdan Doicin <alex.zanardi@gmail.com>
2021-11-12 14:09:38 +0200

    Programare

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ print(raw_data.head())
 
 featured_data = raw_data
 
-# Calculez Last FGRating
+# Calculez Last FGrating
 featured_data['Last FGrating'] = featured_data.groupby('HorseId')['FGrating'].apply(lambda x: x.shift(1))
 
 # Calculez Last Final Position
 featured_data['Last Plassering'] = featured_data.groupby('HorseId')['Plassering'].apply(lambda x: x.shift(1))
+
+# Calculez Last FGrating pentru Sha-Tin - iarba
+featured_data['Last FGrating at Sha-Tin Grass'] = featured_data.groupby(['HorseId', 'Track', 'Surface'])[
+    'FGrating'].apply(lambda x: x.shift(1)) if (
+                                                       featured_data.Track == 'Sha Tin') & (
+                                                           featured_data.Surface == 'Gress') else 2
 
 featured_data.to_excel('Date sortate.xlsx')
