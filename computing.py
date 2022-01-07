75c9637b894e3033615268445e644e15683333dd
Bogdan Doicin <alex.zanardi@gmail.com>
2022-01-07 16:23:17 +0200

    Imbunatatire cod

--- computing.py
+++ computing.py
@@ -174,3 +174,206 @@ def compute_max_fg_ratings_without_conditions(df, mask='', text='Maximum FGratin
 def compute_trainer_win_percent_without_conditions(df, text, time_length='1000D', mask=''):
     df[text] = compute_trainer_win_percent_in_last_days(df, time_length, mask=mask)
     f.fill_for_all(df, text, 'TrainerID')
+
+
+def compute_average_fgratings_in_the_last_10_or_4_starts(df):
+    for i in [10, 4]:
+        text = 'Average FGrating in the last ' + str(i) + ' starts'
+        df[text] = compute_average_fgrating_in_last_starts(df, i)
+        df[text] = f.fill_for_all(df, text, 'HorseId')
+
+
+def compute_average_final_position_in_the_last_10_or_4_starts(df):
+    for i in [10, 4]:
+        text = 'Average Position in the last ' + str(i) + ' starts'
+        df[text] = compute_average_position_in_last_starts(df, i)
+        df[text] = f.fill_for_all(df, text, 'HorseId')
+
+
+def compute_average_fgratings_for_every_track(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_tracks(df, i, 'Average FGrating')
+        compute_average_fgratings_without_conditions(df, mask, text)
+
+
+def compute_average_final_position_for_every_track(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_tracks(df, i, 'Average Position')
+        compute_average_positions_without_conditions(df, mask, text)
+
+
+def compute_average_fgratings_for_every_distance_type(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_distance_types(df, i, 'Average FGrating')
+        compute_average_fgratings_without_conditions(df, mask, text)
+
+
+def compute_average_final_position_for_every_distance_type(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_distance_types(df, i, 'Average Position')
+        compute_average_positions_without_conditions(df, mask, text)
+
+
+def compute_maximum_fgratings_for_every_track(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_tracks(df, i, 'Maximum FGrating')
+        compute_max_fg_ratings_without_conditions(df, mask, text)
+
+
+def compute_maximum_fgratings_for_every_distance_type(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_distance_types(df, i, 'Maximum FGrating')
+        compute_max_fg_ratings_without_conditions(df, mask, text)
+
+
+def compute_maximum_fgratings_in_the_last_3_starts(df):
+    df['Maximum FGrating in last 3 starts'] = df.groupby('HorseId')['FGrating'].apply(
+        lambda x: x.shift().expanding(min_periods=3).max())
+    df['Maximum FGrating in last 3 starts'] = f.fill_for_all(df, 'Maximum FGrating in last 3 starts', 'HorseId')
+
+
+def compute_top_for_horse(df):
+    df['Top'] = ((df['FGrating'] - df['Maximum FGrating'] >= 4).map({True: 1, False: 0}))
+
+
+def compute_days_since_last_race(df):
+    df['Days since last race'] = df.groupby('HorseId')['Dato'].apply(
+        lambda x: x.diff().fillna(method='ffill').fillna(pd.NaT))
+
+
+def compute_trainer_win_percent_in_the_last_days(df):
+    for i in [1000, 90, 30]:
+        text = 'Trainer winning % in the last ' + str(i) + ' days'
+        time_length = str(i) + 'D'
+        compute_trainer_win_percent_without_conditions(df, text, time_length=time_length)
+
+
+def compute_trainer_win_percent_for_every_distance(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_distance_types(df, i, 'Trainer winning % in the last 1000 days')
+        compute_trainer_win_percent_without_conditions(df, text, mask=mask)
+
+
+def compute_trainer_win_percent_for_every_track(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_tracks(df, i, 'Trainer winning % in the last 1000 days')
+        compute_trainer_win_percent_without_conditions(df, text, mask=mask)
+
+
+def compute_jockey_win_percent(df):
+    df['Jockey winning % in the last 1000 days'] = compute_jockey_win_percent_in_last_days(df, '1000D')
+    df['Jockey winning % in the last 1000 days'] = df.groupby('JockeyId')[
+        'Jockey winning % in the last 1000 days'].fillna(method='ffill').fillna(0)
+
+
+def compute_jockey_win_percent_on_tracks_with_and_without_surfaces(df):
+    for i in range(5):
+        mask, text = r.return_mask_and_text_from_tracks(df, i, 'Jockey winning % in the last 1000 days')
+        df[text] = compute_jockey_win_percent_in_last_days(df, '1000D', mask=mask)
+
+
+def compute_jockey_win_percent_on_every_surface(df):
+    for i in [0, 1]:
+        mask, text = r.return_mask_and_text_from_surfaces(df, i, 'Jockey winning % in the last 1000 days')
+        df[text] = compute_jockey_win_percent_in_last_days(df, '1000D', mask=mask)
+
+
+def compute_jockey_win_percent_on_every_track(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_tracks(df, i, 'Jockey winning % in the last 1000 days')
+        df[text] = compute_jockey_win_percent_in_last_days(df, '1000D', mask=mask)
+
+
+def compute_jockey_win_percent_on_every_distance(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_distance_types(df, i, 'Jockey winning % in the last 1000 days')
+        df[text] = compute_jockey_win_percent_in_last_days(df, '1000D', mask=mask)
+
+
+def compute_jockey_win_percent_on_distances_and_surfaces(df):
+    for i in range(3):
+        for j in range(2):
+            mask, text = r.return_mask_and_text_from_distances_and_surfaces(df, i, j,
+                                                                            'Jockey winning % in the last 1000 days')
+            df[text] = compute_jockey_win_percent_in_last_days(df, '1000D', mask=mask)
+
+
+def compute_jockey_average_final_position(df):
+    df['Average Position of a jockey in the last 1000 days'] = compute_jockey_average_final_position_in_last_days(df,
+                                                                                                                  '1000D')
+    df['Average Position of a jockey in the last 1000 days'] = f.fill_for_all(df,
+                                                                              'Average Position of a jockey in the last 1000 days',
+                                                                              'JockeyId')
+
+
+def compute_jockey_average_final_position_on_tracks_disregarding_surfaces(df):
+    for i in [3, 4]:
+        mask, text = r.return_mask_and_text_from_tracks(df, i, 'Average Position of a jockey in the last 1000 days')
+        df.loc[mask, text] = compute_jockey_average_final_position_in_last_days(df, '1000D', mask=mask)
+        df[text] = f.fill_for_all(df, text, 'JockeyId')
+
+
+def compute_jockey_average_final_position_on_surfaces(df):
+    for i in range(2):
+        mask, text = r.return_mask_and_text_from_surfaces(df, i, 'Average Position of a jockey in the last 1000 days')
+        df.loc[mask, text] = compute_jockey_average_final_position_in_last_days(df, '1000D', mask=mask)
+        df[text] = f.fill_for_all(df, text, 'JockeyId')
+
+
+def compute_jockey_average_final_position_on_tracks_and_surfaces(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_tracks(df, i, 'Average Position of a jockey in the last 1000 days')
+        df.loc[mask, text] = compute_jockey_average_final_position_in_last_days(df, '1000D', mask=mask)
+        df[text] = f.fill_for_all(df, text, 'JockeyId')
+
+
+def compute_jockey_average_final_position_for_every_distance_type(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_distance_types(df, i,
+                                                                'Average Position of a jockey in the last 1000 days')
+        df.loc[mask, text] = compute_jockey_average_final_position_in_last_days(df, '1000D', mask=mask)
+        df[text] = f.fill_for_all(df, text, 'JockeyId')
+
+
+def compute_jockey_average_final_position_on_tracks_and_surfaces(df):
+    for i in range(3):
+        for j in range(2):
+            mask, text = r.return_mask_and_text_from_distances_and_surfaces(df, i, j,
+                                                                            'Average Position of a jockey in the last 1000 days')
+            df.loc[mask, text] = compute_jockey_average_final_position_in_last_days(df, '1000D', mask=mask)
+            df[text] = f.fill_for_all(df, text, 'JockeyId')
+
+
+def compute_jockey_mean_path(df):
+    df['Mean path of a jockey in the last 1000 days'] = compute_jockey_mean_path_in_last_days(df, '1000D')
+    df['Mean path of a jockey in the last 1000 days'] = f.fill_for_all(df,
+                                                                       'Mean path of a jockey in the last 1000 days',
+                                                                       'JockeyId')
+
+
+def compute_jockey_mean_path_on_surface(df):
+    for i in range(2):
+        mask, text = r.return_mask_and_text_from_surfaces(df, i, 'Mean path of a jockey in the last 1000 days')
+        df.loc[mask, text] = compute_jockey_mean_path_in_last_days(df, '1000D', mask=mask)
+        df[text] = f.fill_for_all(df, text, 'JockeyId')
+
+
+def compute_jockey_mean_path_on_tracks_and_surfaces(df):
+    for i in range(3):
+        mask, text = r.return_mask_and_text_from_tracks(df, i, 'Mean path of a jockey in the last 1000 days')
+        df.loc[mask, text] = compute_jockey_mean_path_in_last_days(df, '1000D', mask=mask)
+        df[text] = f.fill_for_all(df, text, 'JockeyId')
+
+
+def compute_jockey_mean_path_on_distances_and_surfaces(df):
+    for i in range(3):
+        for j in range(2):
+            mask, text = r.return_mask_and_text_from_distances_and_surfaces(df, i, j,
+                                                                            'Mean path of a jockey in the last 1000 days')
+            df.loc[mask, text] = compute_jockey_mean_path_in_last_days(df, '1000D', mask=mask)
+            df[text] = f.fill_for_all(df, text, 'JockeyId')
+
+
+def compute_horse_win_percent(df):
+    df['Horse winning %'] = compute_horse_win_percentage(df)
+    df['Horse winning %'] = df.groupby('HorseId')['Horse winning %'].fillna(method='ffill').fillna(0)
--- main.py
+++ main.py
@@ -28,204 +28,105 @@ c.compute_average_fgratings_without_conditions(featured_data)
 c.compute_average_positions_without_conditions(featured_data)
 
 # Calculez FGrating mediu in ultimele 10, respectiv 4 starturi pentru fiecare cal
-for i in [10, 4]:
-    text = 'Average FGrating in the last ' + str(i) + ' starts'
-    featured_data[text] = c.compute_average_fgrating_in_last_starts(featured_data, i)
-    featured_data[text] = f.fill_for_all(featured_data, text, 'HorseId')
+c.compute_average_fgratings_in_the_last_10_or_4_starts(featured_data)
 
 # Calculez pozitia finala medie in ultimele 10, respectiv 4 starturi pentru fiecare cal
-for i in [10, 4]:
-    text = 'Average Position in the last ' + str(i) + ' starts'
-    featured_data[text] = c.compute_average_position_in_last_starts(featured_data, i)
-    featured_data[text] = f.fill_for_all(featured_data, text, 'HorseId')
+c.compute_average_final_position_in_the_last_10_or_4_starts(featured_data)
 
 # Calculez FGrating mediu pentru fiecare dintre cele trei piste:Sha Tin Grass, Sha Tin Dirt si Happy Valley Grass
 # pentru fiecare cal
-
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_tracks(featured_data, i, 'Average FGrating')
-    c.compute_average_fgratings_without_conditions(featured_data, mask, text)
+c.compute_average_fgratings_for_every_track(featured_data)
 
 # Calculez pozitia medie pentru fiecare dintre cele trei piste:Sha Tin Grass, Sha Tin Dirt si Happy Valley Grass
 # pentru fiecare cal
-
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_tracks(featured_data, i, 'Average Position')
-    c.compute_average_positions_without_conditions(featured_data, mask, text)
+c.compute_average_final_position_for_every_track(featured_data)
 
 # Calculez FGrating mediu pentru cele trei tipuri de distante pentru fiecare cal
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_distance_types(featured_data, i, 'Average FGrating')
-    c.compute_average_fgratings_without_conditions(featured_data, mask, text)
+c.compute_average_fgratings_for_every_distance_type(featured_data)
 
 # Calculez pozitia medie pentru cele trei tipuri de distante pentru fiecare cal
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_distance_types(featured_data, i, 'Average Position')
-    c.compute_average_positions_without_conditions(featured_data, mask, text)
+c.compute_average_final_position_for_every_distance_type(featured_data)
 
 # Calculez FGrating maxim pentru fiecare cal
 c.compute_max_fg_ratings_without_conditions(featured_data)
 
 # Calculez FGrating maxim pentru cele trei piste:Sha Tin Grass, Sha Tin Dirt si Happy Valley Grass, pentru fiecare cal
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_tracks(featured_data, i, 'Maximum FGrating')
-    c.compute_max_fg_ratings_without_conditions(featured_data, mask, text)
+c.compute_maximum_fgratings_for_every_track(featured_data)
 
 # Calculez FGrating maxim pentru cele trei tipuri de distante, pentru fiecare cal
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_distance_types(featured_data, i, 'Maximum FGrating')
-    c.compute_max_fg_ratings_without_conditions(featured_data, mask, text)
+c.compute_maximum_fgratings_for_every_distance_type(featured_data)
 
 # Calculez FGrating maxim pentru fiecare cal din ultimele trei starturi
-featured_data['Maximum FGrating in last 3 starts'] = featured_data.groupby('HorseId')['FGrating'].apply(
-    lambda x: x.shift().expanding(min_periods=3).max())
-featured_data['Maximum FGrating in last 3 starts'] = f.fill_for_all(featured_data, 'Maximum FGrating in last 3 starts',
-                                                                    'HorseId')
+c.compute_maximum_fgratings_in_the_last_3_starts(featured_data)
 
 # Calculez daca un cal a fost varf de forma intr-o anumita cursa
-featured_data['Top'] = (
-    (featured_data['FGrating'] - featured_data['Maximum FGrating'] >= 4).map({True: 1, False: 0}))
+c.compute_top_for_horse(featured_data)
 
 # Calculez numarul de zile de la ultima cursa pentru fiecare cal
-featured_data['Days since last race'] = featured_data.groupby('HorseId')['Dato'].apply(
-    lambda x: x.diff().fillna(method='ffill').fillna(pd.NaT))
+c.compute_days_since_last_race(featured_data)
 
 # Calculez procentajul de victorii ale unui antrenor in ultimele 1000, 90, respectiv 30 de zile
-for i in [1000, 90, 30]:
-    text = 'Trainer winning % in the last ' + str(i) + ' days'
-    time_length = str(i) + 'D'
-    c.compute_trainer_win_percent_without_conditions(featured_data, text, time_length=time_length)
+c.compute_trainer_win_percent_in_the_last_days(featured_data)
 
 # Calculez procentajul de victorii ale unui antrenor in ultimele 1000 de zile, pe cele trei tipuri de distante
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_distance_types(featured_data, i, 'Trainer winning % in the last 1000 days')
-    c.compute_trainer_win_percent_without_conditions(featured_data, text, mask=mask)
+c.compute_trainer_win_percent_for_every_distance(featured_data)
 
 # Calculez procentajul de victorii ale unui antrenor, in ultimele 1000 de zile, pe cele trei piste
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_tracks(featured_data, i, 'Trainer winning % in the last 1000 days')
-    c.compute_trainer_win_percent_without_conditions(featured_data, text, mask=mask)
+c.compute_trainer_win_percent_for_every_track(featured_data)
 
 # Calculez procentajul de victorii al unui jocheu in ultimele 1000 de zile
-featured_data['Jockey winning % in the last 1000 days'] = c.compute_jockey_win_percent_in_last_days(featured_data,
-                                                                                                    '1000D')
-featured_data['Jockey winning % in the last 1000 days'] = featured_data.groupby('JockeyId')[
-    'Jockey winning % in the last 1000 days'].fillna(method='ffill').fillna(0)
+c.compute_jockey_win_percent(featured_data)
 
 # Calculez procentajul de victorii al unui jocheu in ultimele 1000 de zile pe cele trei piste, precum si pe piste,
 # dar indiferent de suprafata
-for i in range(5):
-    mask, text = r.return_mask_and_text_from_tracks(featured_data, i, 'Jockey winning % in the last 1000 days')
-    featured_data[text] = c.compute_jockey_win_percent_in_last_days(featured_data, '1000D', mask=mask)
-    # featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_win_percent_on_tracks_with_and_without_surfaces(featured_data)
 
 # Calculez procentajul de victorii al unui jocheu in ultimele 1000 de zile pe fiecare suprafata
-for i in [0, 1]:
-    mask, text = r.return_mask_and_text_from_surfaces(featured_data, i, 'Jockey winning % in the last 1000 days')
-    featured_data[text] = c.compute_jockey_win_percent_in_last_days(featured_data, '1000D', mask=mask)
-    # featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_win_percent_on_every_surface(featured_data)
 
 # Calculez procentajul de victorii al unui jocheu in ultimele 1000 de zile pe fiecare pista
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_tracks(featured_data, i, 'Jockey winning % in the last 1000 days')
-    featured_data[text] = c.compute_jockey_win_percent_in_last_days(featured_data, '1000D', mask=mask)
-    # featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_win_percent_on_every_track(featured_data)
 
 # Calculez procentajul de victorii al unui jocheu in ultimele 1000 de zile pe fiecare tip de distanta
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_distance_types(featured_data, i, 'Jockey winning % in the last 1000 days')
-    featured_data[text] = c.compute_jockey_win_percent_in_last_days(featured_data, '1000D', mask=mask)
-    # featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_win_percent_on_every_distance(featured_data)
 
 # Calculez procentajul de victorii al unui jocheu in ultimele 1000 de zile, pe cele trei tipuri de distante
 # si pe cele doua tipuri de suprafete
-for i in range(3):
-    for j in range(2):
-        mask, text = r.return_mask_and_text_from_distances_and_surfaces(featured_data, i, j,
-                                                                        'Jockey winning % in the last 1000 days')
-        featured_data[text] = c.compute_jockey_win_percent_in_last_days(featured_data, '1000D', mask=mask)
-        # featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_win_percent_on_distances_and_surfaces(featured_data)
 
 # Calculez pozitia finala medie a unui jocheu in ultimele 1000 de zile
-featured_data[
-    'Average Position of a jockey in the last 1000 days'] = c.compute_jockey_average_final_position_in_last_days(
-    featured_data, '1000D')
-featured_data['Average Position of a jockey in the last 1000 days'] = f.fill_for_all(featured_data,
-                                                                                     'Average Position of a jockey in the last 1000 days',
-                                                                                     'JockeyId')
+c.compute_jockey_average_final_position(featured_data)
 
 # Calculez pozitia finala medie a unui jocheu in ultimele 1000 de zile pe cele doua piste, indiferent de suprafata
-for i in [3, 4]:
-    mask, text = r.return_mask_and_text_from_tracks(featured_data, i,
-                                                    'Average Position of a jockey in the last 1000 days')
-    featured_data.loc[mask, text] = c.compute_jockey_average_final_position_in_last_days(featured_data, '1000D',
-                                                                                         mask=mask)
-    featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_average_final_position_on_tracks_disregarding_surfaces(featured_data)
 
 # Calculez pozitia finala medie a unui jocheu in ultimele 1000 de zile pe cele doua suprafete
-for i in range(2):
-    mask, text = r.return_mask_and_text_from_surfaces(featured_data, i,
-                                                      'Average Position of a jockey in the last 1000 days')
-    featured_data.loc[mask, text] = c.compute_jockey_average_final_position_in_last_days(featured_data, '1000D',
-                                                                                         mask=mask)
-    featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_average_final_position_on_surfaces(featured_data)
 
 # Calculez pozitia finala medie a unui jocheu in ultimele 1000 de zile pe cele 2 piste, inclusiv suprafetele
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_tracks(featured_data, i,
-                                                    'Average Position of a jockey in the last 1000 days')
-    featured_data.loc[mask, text] = c.compute_jockey_average_final_position_in_last_days(featured_data, '1000D',
-                                                                                         mask=mask)
-    featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_average_final_position_on_tracks_and_surfaces(featured_data)
 
 # Calculez pozitia finala medie a unui jocheu in ultimele 1000 de zile pe cele trei tipuri de distante
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_distance_types(featured_data, i,
-                                                            'Average Position of a jockey in the last 1000 days')
-    featured_data.loc[mask, text] = c.compute_jockey_average_final_position_in_last_days(featured_data, '1000D',
-                                                                                         mask=mask)
-    featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_average_final_position_for_every_distance_type(featured_data)
 
 # Calculez pozitia finala medie a unui jocheu in ultimele 1000 de zile pe cele trei tipuri de distante si pe cele doua
 # tipuri de suprafata
-for i in range(3):
-    for j in range(2):
-        mask, text = r.return_mask_and_text_from_distances_and_surfaces(featured_data, i, j,
-                                                                        'Average Position of a jockey in the last 1000 days')
-        featured_data.loc[mask, text] = c.compute_jockey_average_final_position_in_last_days(featured_data, '1000D',
-                                                                                             mask=mask)
-        featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_average_final_position_on_tracks_and_surfaces(featured_data)
 
 # Calculez calea ('Path') medie a unui jocheu in ultimele 1000 de zile
-featured_data['Mean path of a jockey in the last 1000 days'] = \
-    c.compute_jockey_mean_path_in_last_days(featured_data, '1000D')
-featured_data['Mean path of a jockey in the last 1000 days'] = f.fill_for_all(featured_data,
-                                                                              'Mean path of a jockey in the last 1000 days',
-                                                                              'JockeyId')
+c.compute_jockey_mean_path(featured_data)
 
 # Calculez calea ('Path') medie a unui jocheu in ultimele 1000 de zile, in functie de suprafata
-for i in range(2):
-    mask, text = r.return_mask_and_text_from_surfaces(featured_data, i, 'Mean path of a jockey in the last 1000 days')
-    featured_data.loc[mask, text] = c.compute_jockey_mean_path_in_last_days(featured_data, '1000D', mask=mask)
-    featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_mean_path_on_surface(featured_data)
 
 # Calculez calea ('Path') medie a unui jocheu in ultimele 1000 de zile, in functie de pista si suprafata
-for i in range(3):
-    mask, text = r.return_mask_and_text_from_tracks(featured_data, i, 'Mean path of a jockey in the last 1000 days')
-    featured_data.loc[mask, text] = c.compute_jockey_mean_path_in_last_days(featured_data, '1000D', mask=mask)
-    featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_mean_path_on_tracks_and_surfaces(featured_data)
 
 # Calculez calea ('Path') medie a unui jocheu in ultimele 1000 de zile, in functie de suprafata si distanta
-for i in range(3):
-    for j in range(2):
-        mask, text = r.return_mask_and_text_from_distances_and_surfaces(featured_data, i, j,
-                                                                        'Mean path of a jockey in the last 1000 days')
-        featured_data.loc[mask, text] = c.compute_jockey_mean_path_in_last_days(featured_data, '1000D', mask=mask)
-        featured_data[text] = f.fill_for_all(featured_data, text, 'JockeyId')
+c.compute_jockey_mean_path_on_distances_and_surfaces(featured_data)
 
 # Calculez procentajul de victorii ale unui cal
-featured_data['Horse winning %'] = c.compute_horse_win_percentage(featured_data)
-featured_data['Horse winning %'] = featured_data.groupby('HorseId')['Horse winning %'].fillna(method='ffill').fillna(0)
+c.compute_horse_win_percent(featured_data)
 
 featured_data = featured_data.drop(columns='cumsum')
 featured_data = featured_data.drop(columns='Win')
